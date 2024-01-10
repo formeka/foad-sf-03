c4abc41b05f91a47aecc5c600ed7bc70464b027d
Elmira PETIT <elledevelope@gmail.com>
2024-01-10 19:27:44 +0100

    Created HomeController with filter of 'formations' by 'duree'

--- sfformations/src/Controller/HomeController.php
+++ sfformations/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FormationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(FormationsRepository $formationRepository): Response
     {
+        $formations = $formationRepository->findBy([], ['duree' => 'ASC']);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'formations' => $formations,
         ]);
+
     }
 }
