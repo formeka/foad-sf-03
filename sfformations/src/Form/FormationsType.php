069c0842bcc455941c5e86adf7dc46c4a700c737
Elmira PETIT <elledevelope@gmail.com>
2024-01-10 17:48:18 +0100

    Created CRUD for Formations

---
+++ sfformations/src/Form/FormationsType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formations;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('resume')
+            ->add('description')
+            ->add('duree')
+            ->add('niveau')
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formations::class,
+        ]);
+    }
+}
